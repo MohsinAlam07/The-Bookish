8cf8366034254664e362efac1feceea3c6ee523d
Md Mohsin Alam <mohsinalam1141@gmail.com>
2026-03-31 11:41:09 +0600

    navbar and footer

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import Root from '../components/pages/Root/Root';
+import ErrorPage from '../components/pages/ErrorPage/ErrorPage';
+import Home from '../components/pages/Home/Home';
 
 
 export const router = createBrowserRouter([
   {
     path: "/",
     Component: Root,
+    errorElement:<ErrorPage></ErrorPage>,
+    children:[
+        {
+            index:true,
+            path:"/",
+            Component:Home
+        }
+    ]
   },
 ]);
