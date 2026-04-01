3bc675c33502e73869e101de7c2bf41fb417f712
Md Mohsin Alam <mohsinalam1141@gmail.com>
2026-04-01 23:51:02 +0600

    wishlit and read

--- src/components/pages/BookDetails/BookDetails.jsx
+++ src/components/pages/BookDetails/BookDetails.jsx
@@ -3,13 +3,20 @@ import { useLoaderData, useParams } from 'react-router';
 
 const BookDetails = () => {
     const {id}=useParams();
+    const bookId=parseInt(id);
+  
 const data=useLoaderData();
-console.log(data)
+const singleBook=data.find(book=>book.bookId===bookId);
+// console.log(singleBook);
+// console.log(id)
+const {bookName,image}=singleBook;
+
     return (
-        <div>
-            <h1>bookdetails
-                
-            </h1>
+        <div className=' w-2/3 mx-auto'>
+            <img className='w-48' src={image} alt="" />
+            <h5>{bookName}</h5>
+            <button className='btn btn-accent m-2'>Read</button>
+            <button className='btn btn-info m-2'>Wishlist</button>
         </div>
     );
 };
