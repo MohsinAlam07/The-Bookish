eb7fe28681cf998de4e66fb6e76c9ca43c62b8a8
Md Mohsin Alam <mohsinalam1141@gmail.com>
2026-04-02 19:01:20 +0600

    sort by rating and pages

--- src/components/pages/ReadList/ReadList.jsx
+++ src/components/pages/ReadList/ReadList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
@@ -6,6 +6,19 @@ import { getStoredBook } from '../../../utility/addToDB';
 
 const ReadList = () => {
 const [readList,setReadList]=useState([]);
+const [sort,setSort]=useState("");
+const handleSort=(type)=>{
+  setSort(type);
+  if(type==="pages"){
+    const sortedByPages=[...readList].sort((a,b)=>a.totalPages-b.totalPages);
+    setReadList(sortedByPages);
+  }
+  if(type==="ratings"){
+    const sortedByRatings=[...readList].sort((a,b)=>a.rating-b.rating);
+    setReadList(sortedByRatings);
+  }
+
+}
 
     const data=useLoaderData();
     // console.log(data)
@@ -17,6 +30,13 @@ const [readList,setReadList]=useState([]);
     },[])
     return (
         <div>
+          <details className="dropdown">
+  <summary className="btn m-1">Sort By:{sort?sort:""}</summary>
+  <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+    <li><a onClick={()=>handleSort("pages")}>Pages</a></li>
+    <li><a onClick={()=>handleSort("ratings")}>Rating</a></li>
+  </ul>
+</details>
             <Tabs>
     <TabList>
       <Tab>Read List</Tab>
@@ -24,7 +44,17 @@ const [readList,setReadList]=useState([]);
     </TabList>
 
     <TabPanel>
-      <h2>Book i Read{readList.length}</h2>
+      <h2>Book i Read: {readList.length}</h2>
+        {
+    readList.map(book => (
+      <div key={book.bookId} className="border p-4 my-3 rounded">
+        <h3 className="text-xl font-bold">{book.bookName}</h3>
+        <p>Author: {book.author}</p>
+        <img src={book.image} alt="" className="w-32" />
+      </div>
+    ))
+  }
+     
     </TabPanel>
     <TabPanel>
       <h2>Wish List</h2>
