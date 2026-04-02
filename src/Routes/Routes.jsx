70d0908d35b446a399b64496963fafbbf390a3e1
Md Mohsin Alam <mohsinalam1141@gmail.com>
2026-04-02 10:40:20 +0600

    wishlist

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -27,6 +27,7 @@ export const router = createBrowserRouter([
         },
         {
           path:'readList',
+           loader: ()=>fetch('bookData.json'),
           element:<ReadList></ReadList>
 
         },
--- src/components/pages/ReadList/ReadList.jsx
+++ src/components/pages/ReadList/ReadList.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLoaderData } from 'react-router';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import { getStoredBook } from '../../../utility/addToDB';
 
 const ReadList = () => {
+const [readList,setReadList]=useState([]);
+
+    const data=useLoaderData();
+    // console.log(data)
+    useEffect(()=>{
+        const storedBookData=getStoredBook();
+        const ConvertedStoredBooks=storedBookData.map(id=>parseInt(id));
+        const myReadList=data.filter(book=>ConvertedStoredBooks.includes(book.bookId));
+        setReadList(myReadList);
+    },[])
     return (
         <div>
-            <h1>ReadList</h1>
+            <Tabs>
+    <TabList>
+      <Tab>Read List</Tab>
+      <Tab>Wish List</Tab>
+    </TabList>
+
+    <TabPanel>
+      <h2>Book i Read{readList.length}</h2>
+    </TabPanel>
+    <TabPanel>
+      <h2>Wish List</h2>
+    </TabPanel>
+  </Tabs>
         </div>
     );
 };
